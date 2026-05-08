ab1323de4af665fe8c7dd4408d8d0157f73bd5fe
gerard.tous <gerard.tous@GERARDINFO-2025>
2026-05-08 15:33:16 +0200

    cambios

--- DarwinGA/MyRandom.cs
+++ DarwinGA/MyRandom.cs
@@ -8,79 +8,30 @@ namespace DarwinGA
 {
     public static class MyRandom
     {
-        private static readonly object _sync = new();
-        private static Random _rand = Random.Shared;
-
-        public static void SetSeed(int seed)
-        {
-            lock (_sync)
-            {
-                _rand = new Random(seed);
-            }
-        }
-
-        public static void ResetShared()
-        {
-            lock (_sync)
-            {
-                _rand = Random.Shared;
-            }
-        }
-
-        public static T Synchronized<T>(Func<T> action)
-        {
-            lock (_sync)
-            {
-                return action();
-            }
-        }
-
-        public static void Synchronized(Action action)
-        {
-            lock (_sync)
-            {
-                action();
-            }
-        }
-
+        private static Random _rand => Random.Shared;
         public static int NextInt(int minValue, int maxValue)
         {
-            lock (_sync)
-            {
-                return _rand.Next(minValue, maxValue);
-            }
+            return _rand.Next(minValue, maxValue);
         }
 
         public static double NextDouble(double minValue, double maxValue)
         {
-            lock (_sync)
-            {
-                return _rand.NextDouble() * (maxValue - minValue) + minValue;
-            }
+            return _rand.NextDouble() * (maxValue - minValue) + minValue;
         }
 
         public static double NextDouble()
         {
-            lock (_sync)
-            {
-                return _rand.NextDouble();
-            }
+            return _rand.NextDouble();
         }
 
         public static bool NextBool()
         {
-            lock (_sync)
-            {
-                return _rand.NextDouble() > 0.5;
-            }
+            return _rand.NextDouble() > 0.5;
         }
 
         public static int NextInt(int maxValue)
         {
-            lock (_sync)
-            {
-                return _rand.Next(maxValue);
-            }
+            return _rand.Next(maxValue);
         }
 
 
